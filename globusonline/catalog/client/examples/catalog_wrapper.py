00e152478b3d2653170c768f567117fc1de9a4a1
Blaiszik <blaiszik@uchicago.edu>
2013-09-20 16:07:56 -0500

    Style changes re: Bryce Allen code review

--- globusonline/catalog/client/examples/catalog_wrapper.py
+++ globusonline/catalog/client/examples/catalog_wrapper.py
@@ -13,30 +13,24 @@ class CatalogWrapper:
         self.password        = password
         self.token           = token
         self.token_file      = token_file  
-
         self.catalogs        = ''
         self.datasets        = ''
         self.members         = ''
         self.transfer_details = []
-       
-        self.catalogBaseURL  = "https://catalog-alpha.globuscs.info/service/dataset"
-
+        self.catalog_base_url  = "https://catalog-alpha.globuscs.info/service/dataset"
         #Transfer Related Variables
-        self.transferBaseURL = "https://transfer.test.api.globusonline.org/v0.10"
+        self.transfer_base_url = "https://transfer.test.api.globusonline.org/v0.10"
         self.destination_endpoint = ''
         self.destination_path   ='~'
         self.submission_id = ''
         self.transfer = ''
         self.active_endpoints = ''
         self.transfer_queue = ''
-        
         #Client Variables
         self.catalogClient  = ''     #client for interfacing with Globus Catalog
-
+        
         self.GO_authenticate()
-        if(self.token):
-            self.catalogClient = DatasetClient(self.token, self.catalogBaseURL)
-            self.transferClient = TransferAPIClient(self.username, goauth=self.token, base_url=self.transferBaseURL)
+        
 
     #Member Functions      
     def GO_authenticate(self):
@@ -61,6 +55,10 @@ class CatalogWrapper:
             self.token = tmpToken.token  
             self.username = self.token.split('|')[0][3:] #read the username from the token file which is split by | and listed after un= in the first string          
 
+        if(self.token):
+            self.catalogClient = DatasetClient(self.token, self.catalog_base_url)
+            self.transferClient = TransferAPIClient(self.username, goauth=self.token, base_url=self.transfer_base_url)
+
     def check_authenticate(self):
         #This could be a more robust check if necessary
         if self.token != '':
@@ -79,30 +77,18 @@ class CatalogWrapper:
     def set_destination_endpoint(self, destination_endpoint=None):
         self.destination_endpoint = destination_endpoint
         return True
-
-    def load_catalogs(self):
-        _,self.catalogs = self.catalogClient.get_catalogs()
-        return True
-
-    def load_datasets(self, catalog_id):
-        _,self.datasets = self.catalogClient.get_datasets(catalog_id)
-        return True
-
-    def load_members(self, catalog_id, dataset_id):
-        _,self.members = self.catalogClient.get_members(catalog_id,dataset_id)
-        return True
     
     def transfer_members(self, catalog_id, dataset_id, local_path=None):
         _,self.members = self.catalogClient.get_members(catalog_id,dataset_id)
-        self.extract_transfer_details()
-        self.activate_endpoints()
+        self.transfer_details = self.extract_transfer_details(self.members)
+        self.transfer_queue = self.group_transfers(self.transfer_details)
+        self.activate_endpoints(self.transfer_details)
 
         for bundle in self.transfer_queue:
             print '==Unwrapping transfer bundle and creating transfer instance/ID %s => %s=='%(bundle[0]['endpoint'],self.destination_endpoint)
             #code, reason, result = self.transferClient.transfer_submission_id()
             #submission_id = result["value"]
-            #transfer_object = Transfer(submission_id, bundle[0]['endpoint'], self.destination_endpoint)
-            
+            #transfer_object = Transfer(submission_id, bundle[0]['endpoint'], self.destination_endpoint)     
             for transfer in bundle:
                 print '==Starting transfer of %s @%s from %s to %s %s=='%(transfer['type'],transfer['location'],
                                                                           transfer['endpoint'],self.destination_endpoint, 
@@ -123,32 +109,34 @@ class CatalogWrapper:
             #transfer the bundle
         return True
 
-    def extract_transfer_details(self):
+    def extract_transfer_details(self, member_list):
         #get the endpoints and data locations from the data_uri and store them in transfer_details    
+        transfer_details = []
         print '==Extracting Transfer Details=='
-        for member in self.members:
+        for member in member_list:
             tmpDict = {}
             match = re.findall('globus://([\w#]+)/([\w/~._-]+)',member['data_uri'])
             if match != []:
                 tmpDict['endpoint'] = match[0][0]
                 tmpDict['location'] = match[0][1]
                 tmpDict['type']     = member['data_type']
-                self.transfer_details.append(tmpDict)
-        self.group_transfers()
-        return True
+                transfer_details.append(tmpDict)
+        return transfer_details
 
-    def group_transfers(self):
+    def group_transfers(self, transfer_details):
         grouped_transfers = collections.defaultdict(list)
         print '==Grouping Transfers=='
-        for detail in self.transfer_details:
+        for detail in transfer_details:
             grouped_transfers[detail['endpoint']].append(detail)
-        self.transfer_queue = grouped_transfers.values()
+        return grouped_transfers.values()
 
-    def activate_endpoints(self):
+    def activate_endpoints(self, transfer_details):
         unique_endpoints = set()
         unique_endpoints.add(self.destination_endpoint)
-        for detail in self.transfer_details:
+        for detail in transfer_details:
             unique_endpoints.add(detail['endpoint'])
         for endpoint in unique_endpoints:
             print "==Activating Endpoint",endpoint,"=="
             status, message, data = self.transferClient.endpoint_autoactivate(endpoint)
+
+
