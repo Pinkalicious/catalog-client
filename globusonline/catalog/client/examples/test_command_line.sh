c15a7f7acceb1b6100b4684cab30f959258ea8a8
Blaiszik <blaiszik@uchicago.edu>
2013-09-27 14:07:06 -0500

    Changed 'tag' to 'annotation' - Debug now prints to file instead of console

--- globusonline/catalog/client/examples/test_command_line.sh
+++ globusonline/catalog/client/examples/test_command_line.sh
@@ -3,24 +3,19 @@ echo "==Checking get_catalogs=="
 python catalog.py get_catalogs
 python catalog.py -text get_catalogs
 
-
 echo "==Checking get_datsets=="
 python catalog.py get_datasets 17
 python catalog.py -text get_datasets 17
 
-
 #echo "Checking CREATE Catalog"
 #python catalog.py create_catalog '{"config": {"name": "*DELETE* SH Script TEST"}}'
-
 
 echo "==Checking CREATE Dataset=="
 python catalog.py create_dataset 17 '{"name":"New Dataset"}'
 
-
 echo "==Checking get_dataset_members=="
 python catalog.py get_dataset_members 17 54
 python catalog.py -text get_dataset_members 17 54
-
 
 echo "==Checking get_dataset_annotations=="
 python catalog.py get_dataset_annotations 17 100  
